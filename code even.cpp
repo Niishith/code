18a5c733759b7b829023032c6dfc2ef30f674246
Niishith <88286295+Niishith@users.noreply.github.com>
2021-08-02 18:06:23 -0400

    Update code even.cpp

--- code even.cpp
+++ code even.cpp
@@ -4,7 +4,7 @@ int main()    // only int type ex:-2,3,5//
         int num;
     printf("Enter an integer: ");
     scanf("%d", &num);
-    if(num % 2 == 0)
+    if(num % 2 == 0)     //logic:-if the given number is divided by 2  and get reminder as 0 then it is even number else it is odd//
         printf("%d is even.", num);
     else
         printf("%d is odd.", num);
